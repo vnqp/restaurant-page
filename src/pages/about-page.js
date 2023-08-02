08904eb15482788692bb961afb63bca4272dcf3c
Vinícius Moreno <sviniciusmoreno@gmail.com>
2023-08-02 19:39:32 -0300

    Refactor page loaders

--- src/pages/about-page.js
+++ src/pages/about-page.js
@@ -1,58 +1,62 @@
 export default function loadAboutPage () {
   const mainContentDiv = document.getElementById('content');
   mainContentDiv.innerHTML = '';
-
+  {
     const aboutContentDiv = document.createElement('div');
     aboutContentDiv.id = "about-content";
-
+    {
       const aboutContentLeftTitlePlace = document.createElement('div');
       aboutContentLeftTitlePlace.id = "about-content-left-title-place";
-
+      {
         const aboutContentLeftTitle = document.createElement('h1');
         aboutContentLeftTitle.id = 'about-content-left-title';
         aboutContentLeftTitle.innerText = 'About Us!';
+        aboutContentLeftTitlePlace.appendChild(aboutContentLeftTitle);
         
         const aboutContentLeftTitleImgArea = document.createElement('div');
         aboutContentLeftTitleImgArea.id = 'about-content-left-title-img-area';
-          
+        {
           const aboutContentLeftTitleImg1 = document.createElement('div');
           aboutContentLeftTitleImg1.id = 'about-content-left-title-img-1';
           aboutContentLeftTitleImg1.classList.add('about-content-left-title-img');
+
           aboutContentLeftTitleImgArea.appendChild(aboutContentLeftTitleImg1);
 
           const aboutContentLeftTitleImg2 = document.createElement('div');
           aboutContentLeftTitleImg2.id = 'about-content-left-title-img-2';
           aboutContentLeftTitleImg2.classList.add('about-content-left-title-img');
-          aboutContentLeftTitleImgArea.appendChild(aboutContentLeftTitleImg2);
 
-      aboutContentLeftTitlePlace.appendChild(aboutContentLeftTitle);
-      aboutContentLeftTitlePlace.appendChild(aboutContentLeftTitleImgArea);
+          aboutContentLeftTitleImgArea.appendChild(aboutContentLeftTitleImg2);
+        }
+        aboutContentLeftTitlePlace.appendChild(aboutContentLeftTitleImgArea);
+      } 
+      aboutContentDiv.appendChild(aboutContentLeftTitlePlace);
 
       const aboutContentTextPlace = document.createElement('div');
       aboutContentTextPlace.id = 'about-content-text-place';
-        
+      {
         const aboutContentTextTitle = document.createElement('h3');
         aboutContentTextTitle.id = 'about-content-text-title';
         aboutContentTextTitle.innerText = 'Qpower! Elevating Fast Food to Fine Dining!';
+        aboutContentTextPlace.appendChild(aboutContentTextTitle);
 
         const aboutContentText1 = document.createElement('p');
         aboutContentText1.classList.add('about-content-text');
         aboutContentText1.textContent = 'Your one direction to high-quality fast food, serving since 1998 in Brazil. Locals who are proud of the country and want to introduce it to visitors from around the world.';
+        aboutContentTextPlace.appendChild(aboutContentText1);
 
         const aboutContentText2 = document.createElement('p');
         aboutContentText2.classList.add('about-content-text');
         aboutContentText2.textContent = 'We are perfectly positioned to provide our guests with an extraordinary health experience unlike any they’ve ever tasted. We can confidently claim that our menu is unlike anything else offered in the industry.';
+        aboutContentTextPlace.appendChild(aboutContentText2);
 
         const aboutContentText3 = document.createElement('p');
         aboutContentText3.classList.add('about-content-text');
         aboutContentText3.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus mauris nec tincidunt varius. Ut sagittis, lorem at bibendum ornare, mauris neque venenatis justo, ut gravida mi neque eu lorem. Vestibulum auctor leo sit amet vulputate mollis. Curabitur ac lorem felis. Morbi ullamcorper maximus lorem.';
-      
-        aboutContentTextPlace.appendChild(aboutContentTextTitle);
-        aboutContentTextPlace.appendChild(aboutContentText1);
-        aboutContentTextPlace.appendChild(aboutContentText2);
         aboutContentTextPlace.appendChild(aboutContentText3);
-
-    aboutContentDiv.appendChild(aboutContentLeftTitlePlace);
-    aboutContentDiv.appendChild(aboutContentTextPlace);
-  mainContentDiv.appendChild(aboutContentDiv);
+      }
+      aboutContentDiv.appendChild(aboutContentTextPlace);
+    }
+    mainContentDiv.appendChild(aboutContentDiv);
+  }
 }
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,63 +3,65 @@ import loadMenuPage from './menu-page';
 export default function loadHomepage() {
   const contentDiv = document.getElementById('content');
   contentDiv.innerHTML = ''
+  {
+    const hero = document.createElement('div');
+    hero.id = 'hero';
+    {
+      const heroAboutPlace = document.createElement('div');
+      heroAboutPlace.id = 'hero-about-place';
 
-  const hero = document.createElement('div');
-  hero.id = 'hero';
+      const heroAboutTitle = document.createElement('h2');
+      heroAboutTitle.id = 'hero-about-title';
+      heroAboutTitle.textContent = 'Where Culinary Art Meets Speed and Taste.';
+      heroAboutPlace.appendChild(heroAboutTitle);
 
-  const heroAboutPlace = document.createElement('div');
-  heroAboutPlace.id = 'hero-about-place';
+      const heroAboutContent = document.createElement('p');
+      heroAboutContent.id = 'hero-about-content';
+      heroAboutContent.textContent = 'The world of fast food, reimagined, transforming it into an unparalleled dining experience that tantalizes your taste buds with every bite.';
+      heroAboutPlace.appendChild(heroAboutContent);
 
-  const heroAboutTitle = document.createElement('h2');
-  heroAboutTitle.id = 'hero-about-title';
-  heroAboutTitle.textContent = 'Where Culinary Art Meets Speed and Taste.';
-  heroAboutPlace.appendChild(heroAboutTitle);
+      hero.appendChild(heroAboutPlace);
 
-  const heroAboutContent = document.createElement('p');
-  heroAboutContent.id = 'hero-about-content';
-  heroAboutContent.textContent = 'The world of fast food, reimagined, transforming it into an unparalleled dining experience that tantalizes your taste buds with every bite.';
-  heroAboutPlace.appendChild(heroAboutContent);
+      const heroImgPlace = document.createElement('div');
+      heroImgPlace.id = 'hero-img-place';
+      hero.appendChild(heroImgPlace);
 
-  hero.appendChild(heroAboutPlace);
+    }
+    contentDiv.appendChild(hero);
 
-  const heroImgPlace = document.createElement('div');
-  heroImgPlace.id = 'hero-img-place';
-  hero.appendChild(heroImgPlace);
+    const homepageContent = document.createElement('div');
+    homepageContent.id = 'homepage-content';
+    {
+      const homepageContentBorder = document.createElement('div');
+      homepageContentBorder.id = 'homepage-content-border';
+      homepageContent.appendChild(homepageContentBorder)
 
-  contentDiv.appendChild(hero);
+      const homepageContentImgPlace = document.createElement('div');
+      homepageContentImgPlace.id = 'homepage-content-img-place';
+      {
+        const homepageContentImg = document.createElement('div');
+        homepageContentImg.id = 'homepage-content-img';
+        {
+          const homepageContentImgShade = document.createElement('div');
+          homepageContentImgShade.id = 'homepage-content-img-shade';
+            
+          const homepageContentImgButtonPlace = document.createElement('div');
+          homepageContentImgButtonPlace.id = 'homepage-content-img-button-place';
+              
+            const homepageContentButton = document.createElement('button');
+            homepageContentButton.id = 'homepage-content-button';
+            homepageContentButton.textContent = "ORDER NOW!"; 
+            homepageContentButton.onclick = () => {loadMenuPage()};
+            homepageContentImgButtonPlace.appendChild(homepageContentButton);
 
-  const homepageContent = document.createElement('div');
-  homepageContent.id = 'homepage-content';
+          homepageContentImgShade.appendChild(homepageContentImgButtonPlace);
+          homepageContentImg.appendChild(homepageContentImgShade);
+        }
+        homepageContentImgPlace.appendChild(homepageContentImg);
+      }
+      homepageContent.appendChild(homepageContentImgPlace);
+    }
+    contentDiv.appendChild(homepageContent);
+  }
 
-  const homepageContentBorder = document.createElement('div');
-  homepageContentBorder.id = 'homepage-content-border';
-  homepageContent.appendChild(homepageContentBorder)
-
-  const homepageContentImgPlace = document.createElement('div');
-  homepageContentImgPlace.id = 'homepage-content-img-place';
-    
-  const homepageContentImg = document.createElement('div');
-    homepageContentImg.id = 'homepage-content-img';
-      
-    const homepageContentImgShade = document.createElement('div');
-      homepageContentImgShade.id = 'homepage-content-img-shade';
-        
-      const homepageContentImgButtonPlace = document.createElement('div');
-        homepageContentImgButtonPlace.id = 'homepage-content-img-button-place';
-          
-        const homepageContentButton = document.createElement('button');
-          homepageContentButton.id = 'homepage-content-button';
-          homepageContentButton.textContent = "ORDER NOW!"; 
-          homepageContentButton.onclick = () => {loadMenuPage()};
-          homepageContentImgButtonPlace.appendChild(homepageContentButton);
-
-      homepageContentImgShade.appendChild(homepageContentImgButtonPlace);
-
-    homepageContentImg.appendChild(homepageContentImgShade)
-
-  homepageContentImgPlace.appendChild(homepageContentImg);
-
-  homepageContent.appendChild(homepageContentImgPlace);
-
-  contentDiv.appendChild(homepageContent);
 }
